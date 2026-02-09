c78cbe134efff7764433df1ad961161d4ae7d2ef
Eugene Afanasev <emafanasev@yandex.ru>
2026-02-09 16:08:17 +0300

    fix(composable): remove invalid div wrapper from Yandex.Metrika noscript

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -24,7 +24,7 @@ export default defineNuxtPlugin({
 				noscript: [
 					{
 						key: 'yandex-metrika-noscript',
-						innerHTML: `<div><img src="https://mc.yandex.ru/watch/${id}" style="position:absolute; left:-9999px;" alt="" />`
+						innerHTML: `<img src="https://mc.yandex.ru/watch/${id}" style="position:absolute; left:-9999px;" alt="" />`
 					}
 				]
 			});
