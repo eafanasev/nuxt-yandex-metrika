a81e215fd1f557769449006220a2f6f33e4524c5
Eugene Afanasev <emafanasev@yandex.ru>
2026-02-20 11:34:04 +0300

    fix: replace yandex-metrika-noscript to bodyClose position

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -24,7 +24,8 @@ export default defineNuxtPlugin({
 				noscript: [
 					{
 						key: 'yandex-metrika-noscript',
-						innerHTML: `<img src="https://mc.yandex.ru/watch/${id}" style="position:absolute; left:-9999px;" alt="" />`
+						innerHTML: `<img src="https://mc.yandex.ru/watch/${id}" style="position:absolute; left:-9999px;" alt="" />`,
+						tagPosition: 'bodyClose'
 					}
 				]
 			});
